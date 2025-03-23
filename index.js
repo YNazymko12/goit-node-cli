58b3cada8a4b87f273b45860de81bfdb80c793aa
YNazymko12 <yulychka12@gmail.com>
2025-03-23 20:47:57 +0100

    homework is completed

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 import { program } from 'commander';
+import {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+} from './contacts.js';
+
 program
   .option('-a, --action <type>', 'choose action')
   .option('-i, --id <type>', 'user id')
@@ -7,26 +14,36 @@ program
   .option('-p, --phone <type>', 'user phone');
 
 program.parse();
-
 const options = program.opts();
 
-// TODO: рефакторити
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case 'list':
-      // ...
+      console.log('Fetching all contacts...');
+      const contacts = await listContacts();
+      console.table(contacts);
       break;
 
     case 'get':
-      // ... id
+      console.log(`Fetching contact with id=${id}...`);
+      const contact = await getContactById(id);
+      console.table(contact ? contact : `Contact with id=${id} not found`);
       break;
 
     case 'add':
-      // ... name email phone
+      console.log(
+        `Adding new contact: Name=${name}, Email=${email}, Phone=${phone}...`
+      );
+      const newContact = await addContact(name, email, phone);
+      console.log(newContact);
       break;
 
     case 'remove':
-      // ... id
+      console.log(`Removing contact with id=${id}...`);
+      const removedContact = await removeContact(id);
+      console.log(
+        removedContact ? removedContact : `Contact with id=${id} not found`
+      );
       break;
 
     default:
